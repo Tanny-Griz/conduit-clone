003b27f5680891041722442a52caeef6a43e7752
Tanya <tanya.griz89@gmail.com>
2022-11-18 17:09:15 -0600

    added errors page

---
+++ pages/no-access.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="no-access-page">
+    <h1 class="has-text-weight-bold">
+      No Access Page
+    </h1>
+    <p>404</p>
+  </div>
+</template>
+
+<script lang="js">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NoAccessPage',
+  head () {
+    return {
+      titleTemplate: 'No Access Page'
+    }
+  }
+})
+</script>
---
+++ pages/unexpected-error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="no-access-page">
+    <h1 class="has-text-weight-bold">
+      Unexpected Error Page
+    </h1>
+    <p>500</p>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'UnexpectedError',
+  head () {
+    return {
+      titleTemplate: 'Unexpected Error Page'
+    }
+  }
+})
+</script>
